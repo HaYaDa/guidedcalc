5eee87886e510e6db14290d5c23ea5996f9a28ec
H-Sun <hasanyasindagkus@hotmail.com>
2019-11-06 10:38:32 +0100

    Updates

---
+++ precache-manifest.eb626d7b5ab0c3d0195cd69192ae5ada.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a5ba2345944b5737572c625cd8366d",
+    "url": "/guidedcalc/index.html"
+  },
+  {
+    "revision": "57eab1981bcdc1e4596a",
+    "url": "/guidedcalc/static/css/main.74fc019d.chunk.css"
+  },
+  {
+    "revision": "d4c5205172a35eec1ed7",
+    "url": "/guidedcalc/static/js/2.8711b5a5.chunk.js"
+  },
+  {
+    "revision": "57eab1981bcdc1e4596a",
+    "url": "/guidedcalc/static/js/main.fa8e1404.chunk.js"
+  },
+  {
+    "revision": "e44c11190f630873252f",
+    "url": "/guidedcalc/static/js/runtime-main.10a61f21.js"
+  },
+  {
+    "revision": "00fd7ef735de48848a001f4330a96d30",
+    "url": "/guidedcalc/static/media/digital-7.00fd7ef7.woff"
+  },
+  {
+    "revision": "de205a7e92ee0c9153fd56bf208510d0",
+    "url": "/guidedcalc/static/media/digital-7.de205a7e.woff2"
+  }
+]);
